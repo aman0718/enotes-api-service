0bb1d8e344ebc8ecdbfe7dac2a29f641888003ea
aman0718 <aman.soni0718@gmail.com>
2024-11-13 11:34:58 +0530

    category exists implemented

---
+++ src/main/java/com/codework/exception/ExistDataException.java
@@ -0,0 +1,10 @@
+package com.codework.exception;
+
+public class ExistDataException extends RuntimeException {
+
+    public ExistDataException(String message){
+        super(message);
+
+    }
+
+}
